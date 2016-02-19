195aabd42cc62f02c03fef782e9d9566becdce51
Chars Davy <chars_d@126.com>
2016-02-20 00:38:52 +0800

    添加收银台代码

---
+++ JD-Mobile/Mine/Controller/DZCashierViewController.h
@@ -0,0 +1,15 @@
+//
+//  DZCashierViewController.h
+//  JD-Mobile
+//
+//  Created by dengwei on 16/2/20.
+//  Copyright (c) 2016年 dengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZCashierViewController : UITableViewController
+
+- (instancetype)initWithOrderMoney:(float)money;
+
+@end
